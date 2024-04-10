379333da7e0f2e6c83b0d41dde98f6e9b3a38fe5
Mateus Neri <132720769+mateusnerii@users.noreply.github.com>
2024-04-10 17:01:39 -0300

    Create variaveisOperadores.js

---
+++ Atividade 1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let num_a = 30;
+let numb_b = 10;
+
+let resultado = num_a + numb_b;
+console.log('Resultado da soma de ' + num_a + ' e ' + numb_b + ' é igual a:' + resultado);
+
+resultado = num_a - numb_b;
+console.log(`Resultado da subtração de ' ${num_a} ' e ' ${numb_b} ' é igual a:' ${resultado}`);
+
+resultado = num_a * numb_b;
+console.log(`Resultado da multiplicação de ${num_a} e ${numb_b} é igual a: ${resultado}`);
+
+resultado = num_a / numb_b;
+console.log(`Resultado da divisão de ${num_a} e ${numb_b} é igual a: ${resultado}`);
